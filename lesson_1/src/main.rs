7aa93b03aab72f95022de9cff6393ed4225e371c
petraxeman <pashamanprofirus@gmail.com>
2024-06-06 00:15:47 +0300

    Lesson 1 part 2

--- lesson_1/src/main.rs
+++ lesson_1/src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use rand::Rng;
-
+use std::cmp::Ordering;
 
 fn main() {
     println!(">>> GUESSING NUMBER <<<");
@@ -9,7 +9,12 @@ fn main() {
     let answer = rand::thread_rng().gen_range(1..=100);
     let mut choice = String::new();
     io::stdin().read_line(&mut choice).expect("Failed to read line");
-
+    let choice: u32 = choice.trim().parse().expect("Please type a number");
     println!("Right answer {answer}");
     println!("You choice {choice}");
+    match choice.cmp(&answer) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win"),
+    }
 }
