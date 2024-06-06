0a25db178f84e18e2d915f909a0bdee43eea7e7b
petraxeman <pashamanprofirus@gmail.com>
2024-06-06 12:26:04 +0300

    Lesson 1 passed

--- lesson_1/src/main.rs
+++ lesson_1/src/main.rs
@@ -7,14 +7,21 @@ fn main() {
     println!("          ???          ");
 
     let answer = rand::thread_rng().gen_range(1..=100);
-    let mut choice = String::new();
-    io::stdin().read_line(&mut choice).expect("Failed to read line");
-    let choice: u32 = choice.trim().parse().expect("Please type a number");
-    println!("Right answer {answer}");
-    println!("You choice {choice}");
-    match choice.cmp(&answer) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
+
+    loop {
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Failed to read line");
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You choice {choice}");
+
+        match choice.cmp(&answer) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {println!("You win"); break}
+        }
     }
 }
